3066efef8c1f9a7b8817f6a29461d54fc84e337d
Viwongo <157738893+Viwaz@users.noreply.github.com>
2025-11-19 06:09:56 +0200

    Setting up file structure, database and role-based access control for AuntJoy's Restaurant application

---
+++ database.php
@@ -0,0 +1,29 @@
+<?php
+    class Database {
+        private $host = "127.0.0.1";
+        private $port = "3307";
+        private $db_name = "aunt_joydb";
+        private $username = "root";
+        private $password ;
+        protected $conn;
+
+        public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new mysqli(
+                $this->host,
+                $this->username,
+                $this->password,
+                $this->db_name,
+                $this->port
+            );
+            $this->conn->set_charset("set names utf8");
+        } catch(msqli_sql_exception $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+        }
+    }
+?>
---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'auth.php';
+$auth = new Auth();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $role = $auth->login($email,$password);
+
+    if ($role) {
+        switch ($role) {
+            case 'admin': header("Location: admin_dashboard.php"); break;
+            case 'sales': header("Location: sales_dashboard.php"); break;
+            case 'manager': header("Location: manager_dashboard.php"); break;
+            case 'customer': header("Location: index.php"); break;
+        }
+        exit;
+    } else {
+        $error = "Invalid email or password!";
+        echo $error;
+    }
+}
+?>
+<form method="POST">
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
+    <button type="submit">Login</button>
+</form>
+<?php if (isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
